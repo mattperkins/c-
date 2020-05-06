26c8764dfbc7b715ca5b6f0a5fd994744dcbea17
mattperkins <github@mattperkins.com>
2020-05-06 15:23:16 +0100

    init

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+  using std::cout;
+  int slices;
+  slices = 5;
+  cout << "hello,world\n";
+  cout << slices; 
+  cout << "\n";
+  return 0;
+}
+
+void test()
+{
+  std::cout << "test";
+}
