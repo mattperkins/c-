0fd9dcb598da8c18564ebaedb8dc92713f4e2282
mattperkins <github@mattperkins.com>
2020-05-06 16:34:45 +0100

    update

--- test.cpp
+++ test.cpp
@@ -2,22 +2,22 @@
 
 int main()
 {
-  using std::cout;
-  int slices;
-  slices = 5;
-  cout << "The number equals " << slices << " correct" << std::endl;
+    using std::cout;
+    int slices;
+    slices = 5;
+    cout << "The number equals " << slices << " correct" << std::endl;
 
-  printf("%i\n", slices); // alternate logging method 
+    printf("%i\n", slices); // alternate logging method 
 
-  int myNum ;
-  std::cout << "What is your favourite number? ";
-  std::cin >> myNum; 
-  cout << "Your favourite number is " << myNum << std::endl;
+    int myNum;
+    std::cout << "What is your favourite number? ";
+    std::cin >> myNum; 
+    cout << "Your favourite number is " << myNum << std::endl;
 
-  return 0;
+    return 0;
 }
 
 void test()
 {
-  std::cout << "test";
+    std::cout << "test";
 }
