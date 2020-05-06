47ea3e383edfcafc3caabbb042f2a0d6df880a1e
mattperkins <github@mattperkins.com>
2020-05-06 16:30:36 +0100

    update

--- test.cpp
+++ test.cpp
@@ -7,7 +7,11 @@ int main()
   slices = 5;
   cout << "The number equals " << slices << " correct" << std::endl;
 
-  printf("%i\n", slices);
+  printf("%i\n", slices); // alternate logging method 
+
+  int myNum ;
+  std::cin >> myNum; 
+  cout << "The total equals " << myNum << std::endl;
 
   return 0;
 }
