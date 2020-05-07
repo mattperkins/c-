9f7cdd7713cf6ac4ebf48e8618c4314ab1f9d7b1
mattperkins <github@mattperkins.com>
2020-05-08 00:37:48 +0100

    update

---
+++ string_class_and_c_strings.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    /* String Class (C++ standard) */
+    // An instantiated variable string = ""
+    string hello;
+    hello = "hello,world"; // String literal (object instance of the String class - string of characters strung together)
+    cout << hello << endl; // hello,world
+    cout << hello[1] << endl; // e
+    
+    // String concatenation
+    hello += " you are so beautiful";
+    cout << hello + "!" << endl;
+
+    /* methods = member functions attached to objects */
+    cout << hello.length() << endl;
+
+    /* C Strings (backwards compatible with C) */
+    char name[] = "betty"; 
+    cout << name << endl;
+    // a C style string is an array of characters
+    // b,e,t,t,y and \0 (the null terminating character)
+    // if reassigned will display the following error:
+    // array type 'char [6]' is not assignable
+}
