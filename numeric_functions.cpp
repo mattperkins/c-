bc8c27b3aa7c341230df05b38856a18bc4674d92
mattperkins <github@mattperkins.com>
2020-05-07 23:24:57 +0100

    update

--- numeric_functions.cpp
+++ numeric_functions.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
-    
+    cout << sqrt(25) << endl; // 5 (square root of 25)
+    cout << sqrt(-25) << endl; // nan (not a number)
+    cout << pow(9, 999) << endl; // inf (infinity)
+    cout << pow(-9, 999) << endl; // -inf (negative infinity)
+
+    cout << NAN << endl;
+    cout << INFINITY << endl;
+    cout << -INFINITY << endl;
 }
