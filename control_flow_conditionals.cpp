6c3c73ed62a78e637e77fd67badb735f6e4504c6
mattperkins <github@mattperkins.com>
2020-05-10 00:40:43 +0100

    update

--- control_flow_conditionals.cpp
+++ control_flow_conditionals.cpp
@@ -7,32 +7,30 @@ int main()
 {
     // branching -> conditions
         // if statements
-        // switch statements
-
-    /*
+        // switch statements (uses intergral types only i.e Int)
+    
     cout << "How old are you?\n";
     int age;
     cin >> age;
     if(age < 18)
     {
         cout << "You are not old enough" << endl;
-        return  -1;
+        // return  -1;
     }
     else if(age > 30)
     {
         cout << "You are too old" << endl;
-        return  -1;
+        // return  -1;
     } 
     else
     {
         cout << "Welcome to the club!" << endl;
-        return  -1;
+        // return  -1;
     }
-    cout << "This wont display because of return statements above\n";
-    */
+    cout << "This only display when return statements above are active \n";
 
     string answer = "Betty";
-    int age = 50;
+    int guess_age = 50;
 
     string guess;
     cout << "Guess the name\n";
@@ -50,7 +48,7 @@ int main()
     {
         cout << "Correct!\n";
     }
-    else if (age_guess == age) // Equal to 50
+    else if (age_guess == guess_age) // Equal to 50
     {
         cout << "Correct!\n";
     }
@@ -67,6 +65,44 @@ int main()
         {
             // result of both conditions being true
         }
+    }
+
+    int fav_num;
+    cout << "What is the magic number?\n";
+    cin >> fav_num;
+    switch (fav_num)
+    {
+        case 5:
+            cout << "Incorrect\n";
+            break;
+        case 7: // either 7
+        case 8: // or 8
+            cout << "Correct!!!\n";
+            break;
+        case 10:
+            cout << "Incorrect\n";
+            break;
+        default:
+            cout << "The number you chose was wrong\n";
+            break;
+    }    
+
+    // enum class is scoped 
+    // regular enum is not and doesnt require Lottery:: prefix
+    enum class Lottery{one, two, three};
+    Lottery winner = Lottery::one;
+    switch (winner)
+    {
+        case Lottery::one:
+            cout << "Correct!!!\n";
+            break;
+        case Lottery::two:
+            cout << "Wrong\n";
+            break;
+        case Lottery::three:
+            cout << "Wrong\n";
+            break;
+        // default not required here.
     }
     return 0;
 }
